2628df070a68b8902c2c4c2140f293271d94f4a9
Karan Mehta <karanm645@gmail.com>
2021-12-14 15:18:39 -0600

    Create Asc. order method + test

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :movie_performers
   has_many :performers, through: :movie_performers
+
+
 end
--- app/models/performer.rb
+++ app/models/performer.rb
@@ -1,4 +1,8 @@
 class Performer < ApplicationRecord
   has_many :movie_performers
   has_many :movies, through: :movie_performers
+
+  def self.asc_performers
+    order(:age)
+  end
 end
